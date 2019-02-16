07e31e96db1b39675c6044b60ae075944a349a88
Sam Marchant <sam17marchant@gmail.com>
2019-02-16 16:03:52 -0500

    handled IOException

--- src/client.java
+++ src/client.java
@@ -9,6 +9,8 @@ public class client {
             PrintWriter out = new PrintWriter(socket.getOutputStream(), true);
             BufferedReader in = new BufferedReader(new InputStreamReader(socket.getInputStream()));
 
+        } catch (IOException io){
+            System.out.println(io.getMessage());
         }
     }
 }
